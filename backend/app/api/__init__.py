8c06f2516719886a36d97878651e36ed7e3abb0f
wangyuchen <wangyuchen@bj-zxbz.com>
2026-04-08 00:14:36 +1000

    fix the api register part

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,6 +1,8 @@
 def register_blueprints(app):
     from app.api.auth import auth_bp
     from app.api.invitations import invitations_bp
+    from app.api.questionnaire_survey import questionnaire_survey_bp
 
     app.register_blueprint(auth_bp, url_prefix='/api')
     app.register_blueprint(invitations_bp, url_prefix='/api/v1/invitations')
+    app.register_blueprint(questionnaire_survey_bp, url_prefix='/api/v1/questionnaire-surveys')
--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,12 @@
 from app.models.user import User
 from app.models.community import Community
 from app.models.community_member import CommunityMember
+from app.models.questionnaire_survey import (
+    QuestionnaireQuestionBank,
+    QuestionnaireSurvey,
+    QuestionnaireSurveyQuestion,
+    QuestionnaireSurveyResponse,
+    )
+
 
 __all__ = ['User', 'Community', 'CommunityMember']
